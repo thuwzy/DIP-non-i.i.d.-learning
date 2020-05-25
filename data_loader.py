c776b94b93871d38e1991b088aed507c92c0906b
Fanry <fry17@mails.tsinghua.edu.cn>
2020-05-25 11:52:23 +0800

    add dataloader

--- data_loader.py
+++ data_loader.py
@@ -1,10 +1,38 @@
 import numpy as np
+import utils
+import torch
+import torch.utils.data as Data
 
-data = np.load("../course_train.npy")
+def process_data():
+    data = np.load("../course_train.npy")
+    X = torch.Tensor(data[:, :-2])
+    Y = torch.Tensor(np.eye(utils.class_size)[data[:, -2].astype("int32")])
+    C = torch.Tensor(np.eye(utils.context_size)[data[:, -1].astype("int32")])
+    print(X.shape, Y.shape, C.shape)
+    return (X, Y, C)
 
-print("----type----")
-print(type(data))
-print("----shape----")
-print(data.shape)
-print("----data----")
-print(data)
+def load_data():
+    data = np.load("../course_train.npy")
+    X = data[:, :-2]
+    Y = data[:, -2:]
+    X, Y, C = process_data()
+    torch_dataset = Data.TensorDataset(X, Y, C)
+    loader = Data.DataLoader(
+        dataset=torch_dataset,
+        batch_size=utils.batch_size,
+        shuffle=True,
+    )
+    return loader
+
+
+if __name__ == "__main__":
+    data = np.load("../course_train.npy")
+    print("----type----")
+    print(type(data))
+    print("----shape----")
+    print(data.shape)
+    print("----data----")
+    print(data)
+
+    l = load_data()
+    print(l)
--- utils.py
+++ utils.py
@@ -2,3 +2,6 @@ input_size = (3, 64, 64)
 context_size = 10
 class_size = 10
 feature_size = 512
+
+train_size = 4156
+batch_size = 256
